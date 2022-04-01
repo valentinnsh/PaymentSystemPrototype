4c369689b654edb8513d7afe895ad548798dac9d
Valentine Shishkin <valentinnsh@gmail.com>
2022-04-01 20:44:48 +0300

    Fixed User's TransferList ConfirmedBy presentation

--- PaymentSystemPrototype/Pages/UserPages/TransferList.cshtml.cs
+++ PaymentSystemPrototype/Pages/UserPages/TransferList.cshtml.cs
@@ -9,9 +9,12 @@ namespace PaymentSystemPrototype.Pages.UserPages;
 public class TransferList : PageModel
 {
     public List<TransferRecord> UserTransfers;
-    public void OnGet([FromServices] ITransferOperationsService transferOperationsService)
+    public List<UserRecord> UserList; 
+    public void OnGet([FromServices] ITransferOperationsService transferOperationsService,
+        [FromServices] IUserOperationsService userOperationsService)
     {
         UserTransfers = transferOperationsService.GetTransfersForUser(User.Identity.Name);
+        UserList = userOperationsService.GetUsers();
     }
 
     public async Task<IActionResult> OnPostCancel([FromServices] ITransferOperationsService transferOperationsService,
